9d9e29e0ecb5ec324ddc18fca400168533227d4a
Edward Hill <edwardpassanohill@gmail.com>
2020-04-10 17:06:05 -0400

    add react.js, express.js, routes, views

---
+++ ranch-hand-app/client/src/pages/index.js
@@ -0,0 +1,6 @@
+import MoviesList from "./MoviesList";
+import MoviesInsert from "./MoviesInsert";
+import MoviesUpdate from "./MoviesUpdate";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export { MoviesList, MoviesInsert, MoviesUpdate };
