be37e4bcf87cbacc15911f9044451166a4a78e61
Edward Hill <edwardpassanohill@gmail.com>
2020-04-14 13:10:52 -0400

    add models/ herd-model, field-model, ranch-model, users-model- anthony

---
+++ ranch-hand-app/server/models/users-model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const Users = new Schema(
+  {
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("users", Users);
